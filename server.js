c4678377e83e11340927ab19d167b2c821fea1a6
Anand Raj Barnwal <anandrajbarnwal@gmail.com>
2026-01-20 21:59:23 +0530

    hello page added

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const express = require("express")
 const app = express()// server instane create karna
 
 app.get('/', (req,res) => {
-    res.send("Jello World")
+    res.send("Hello World")
 })
 
 app.get('/about', (req,res) => {
     res.send("This is about page")
 })
+
+app.get('/home', (req,res) => {
+    res.send("This is Home page")
+})
+
 app.listen(3000)// server start karna
 
